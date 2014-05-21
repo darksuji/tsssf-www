9bb9a474dbfa547665a87393d15fb443151d5ac9
darksuji <darksuji@gmail.com>
2014-05-20 19:15:07 -0700

    the crudest of game boards, now with legit cards

--- tsssf-www.pl
+++ tsssf-www.pl
@@ -25,20 +25,22 @@ __DATA__
 TSSSF crude-as-balls gameboard
 <div id="hand">
     <img class="pony card"
-        src="https://dl.dropboxusercontent.com/u/68743442/freedom-fighter-pinkie-pie.png">
+        src="https://dl.dropboxusercontent.com/u/68743442/TSSSF%20Low%20Rez/Pony%20-%20Smarty%20Pants.png">
     <img class="pony card"
-        src="https://dl.dropboxusercontent.com/u/68743442/freedom-fighter-pinkie-pie.png">
+        src="https://dl.dropboxusercontent.com/u/68743442/TSSSF%20Low%20Rez/Pony%20-%20Queen%20Chrysalis.png">
     <img class="pony card"
-        src="https://dl.dropboxusercontent.com/u/68743442/freedom-fighter-pinkie-pie.png">
+        src="https://dl.dropboxusercontent.com/u/68743442/TSSSF%20Low%20Rez/Pony%20-%20Princess%20Cadence.png">
     <img class="pony card"
-        src="https://dl.dropboxusercontent.com/u/68743442/freedom-fighter-pinkie-pie.png">
-    <img class="pony card"
-        src="https://dl.dropboxusercontent.com/u/68743442/freedom-fighter-pinkie-pie.png">
-    <img class="goal card"
-        src="https://dl.dropboxusercontent.com/u/68743442/multiply.png">
+        src="https://dl.dropboxusercontent.com/u/68743442/TSSSF%20Low%20Rez/Pony%20-%20Cheerilee.png">
+    <img class="ship card"
+        src="https://dl.dropboxusercontent.com/u/68743442/TSSSF%20Low%20Rez/Ship%20-%20Cult%20Meeting.png">
+    <img class="ship card"
+        src="https://dl.dropboxusercontent.com/u/68743442/TSSSF%20Low%20Rez/Ship%20-%20I%20Read%20That%20In%20A%20Book%20Once.png">
+    <img class="ship card"
+        src="https://dl.dropboxusercontent.com/u/68743442/TSSSF%20Low%20Rez/Ship%20-%20There%20Are%20No%20Breaks%20On%20The%20Love%20Train.png">
 </div>
 
-<table id="gameboard">
+<table id="gameboard" width="100%">
 % for my $row_index (1 .. 7) {
     <tr>
 %   for my $col_index (1 .. 7) {
@@ -56,19 +58,40 @@ TSSSF crude-as-balls gameboard
 %               $type = 'no space';
 %           }
 %       }
-        <td class="<%= $type %>"><%= $row_index %> - <%= $col_index %></td>
+        <td class="<%= $type %>">
+%           if ($type eq 'pony space') {
+                <img src="https://dl.dropboxusercontent.com/u/68743442/TSSSF%20Low%20Rez/Back%20-%20Pony.png">
+%           } elsif ($type eq 'ship space') {
+                <img src="https://dl.dropboxusercontent.com/u/68743442/TSSSF%20Low%20Rez/Back%20-%20Ship.png">
+%           }
+%#        <%= $row_index %> - <%= $col_index %>
+        </td>
 %   }
     </tr>
 % }
 <script>
 $( ".card" ).draggable();
+
 $( ".space" ).filter(" .pony" ).droppable(
     {
-        accept: "[class~='card'][class~='pony']"
+        accept:     "[class~='card'][class~='pony']",
+        tolerance:  "pointer",
+        hoverClass: "hovering",
+        drop:       function (event, ui) {
+            alert("Gooooooooal!");
+        }
     }
 );
-//$( ".space" ).filter(" .ship" ).html('SHIP SPACE');
-$( "td[class~='space'][class~='ship']" ).html('SHIP SPACE');
+$( ".space" ).filter(" .ship" ).droppable(
+    {
+        accept:     "[class~='card'][class~='ship']",
+        tolerance:  "pointer",
+        hoverClass: "hovering",
+        drop:       function (event, ui) {
+            alert("Gooooooooal!");
+        }
+    }
+);
 </script>
 
 @@ layouts/default.html.ep
@@ -78,6 +101,11 @@ $( "td[class~='space'][class~='ship']" ).html('SHIP SPACE');
         <title><%= title %></title>
         <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.min.js"></script>
+
+        <style>
+            td { color: blue }
+            td.hovering { color: red }
+        </style>
     </head>
     <body><%= content %></body>
 </html>
