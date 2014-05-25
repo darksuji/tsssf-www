762d51c8494e339b82fd5cb81a563e119fd42a45
darksuji <darksuji@gmail.com>
2014-05-25 02:18:37 -0700

    renders board from JS data rather than static HTML

--- tsssf-www.pl
+++ tsssf-www.pl
@@ -16,6 +16,40 @@ get '/api/player/:id' => sub {
 get '/api/game/:id' => sub {
 };
 
+=pod
+
+post '/api/card-placement' => sub {
+    my ($self) = @_;
+    my $card_ID = $self->param('card_ID');
+    my $position = $self->param('position');
+
+
+};
+
+=cut
+
+get '/api/board' => sub {
+    my ($self) = @_;
+
+    my $size = 7;
+    my @grid = map {
+        my $row_i = $_;
+        [
+            map {
+                $row_i % 2
+                    ? $_ % 2 ? 'P' : 'S' # pony row
+                    : $_ % 2 ? 'S' : ' ' # ship row
+            } (1 .. $size) 
+        ]
+    } (1 .. $size);
+
+    $self->render(
+        json => {
+            grid => \@grid,
+        },
+    );
+};
+
 post '/api/board' => sub {
     my ($self) = @_;
     my $card = uri_unescape($self->param('card'));
@@ -53,63 +87,90 @@ TSSSF crude-as-balls gameboard
 </div>
 
 <table id="gameboard" width="100%">
-% for my $row_index (1 .. 7) {
-    <tr>
-%   for my $col_index (1 .. 7) {
-%       my $type;
-%       if ($row_index % 2) {
-%           if ($col_index % 2) {
-%               $type = 'pony space';
-%           } else {
-%               $type = 'ship space';
-%           }
-%       } else {
-%           if ($col_index % 2) {
-%               $type = 'ship space';
-%           } else {
-%               $type = 'no space';
-%           }
-%       }
-        <td class="<%= $type %>">
-%           if ($type eq 'pony space') {
-                <img src="https://dl.dropboxusercontent.com/u/68743442/TSSSF%20Low%20Rez/Back%20-%20Pony.png">
-%           } elsif ($type eq 'ship space') {
-                <img src="https://dl.dropboxusercontent.com/u/68743442/TSSSF%20Low%20Rez/Back%20-%20Ship.png">
-%           }
-%#        <%= $row_index %> - <%= $col_index %>
-        </td>
-%   }
-    </tr>
-% }
+</table>
+
 <script>
-$( ".card" ).draggable();
 
-$( ".space" ).filter(" .pony" ).droppable(
-    {
-        accept:     "[class~='card'][class~='pony']",
-        tolerance:  "pointer",
-        hoverClass: "hovering",
-        drop:       function(event, ui) {
-            var uri = ui.draggable.attr("src");
+var my_template_format = function() {
+  var s = arguments[0];
+  for (var i = 0; i < arguments.length - 1; i++) {       
+    var reg = new RegExp("\\{" + i + "\\}", "gm");             
+    s = s.replace(reg, arguments[i + 1]);
+  }
 
-            jQuery.post("/api/board", {card: uri},
-                function(data, textStatus, jqXHR) {
-                    alert("Pony dropped:  " + data);
-                }
+  return s;
+};
+
+var empty_grid_cell_template = '<td class="{0}"></td>';
+var grid_cell_template = '<td class="{0}"><img src="{1}"></td>';
+
+var pony_card_URI = "https://dl.dropboxusercontent.com/u/68743442/TSSSF%20Low%20Rez/Back%20-%20Pony.png";
+var ship_card_URI = "https://dl.dropboxusercontent.com/u/68743442/TSSSF%20Low%20Rez/Back%20-%20Ship.png";
+
+$( document ).ready(function() {
+    var grid = [["P","S","P","S","P","S","P"],["S"," ","S"," ","S"," ","S"],["P","S","P","S","P","S","P"],["S"," ","S"," ","S"," ","S"],["P","S","P","S","P","S","P"],["S"," ","S"," ","S"," ","S"],["P","S","P","S","P","S","P"]];
+
+    var table = $( "#gameboard" );
+    for (var row_i = 0; row_i < grid.length; ++row_i) {
+        var row = $('<tr></tr>');
+        var grid_row = grid[row_i];
+        for (var col_i = 0; col_i < grid_row.length; ++col_i) {
+            var cell_type = (
+                  grid_row[col_i] == "P" ? "pony space"
+                : grid_row[col_i] == "S" ? "ship space"
+                                         : "no space"
             );
+
+            if (cell_type == "no space") {
+                row.append(
+                    my_template_format(
+                        empty_grid_cell_template, cell_type
+                    )
+                );
+            } else {
+                row.append(
+                    my_template_format(
+                        grid_cell_template,
+                        cell_type,
+                        cell_type == "pony space" ? pony_card_URI : ship_card_URI
+                    )
+                );
+            }
         }
+
+        table.append(row);
     }
-);
-$( ".space" ).filter(" .ship" ).droppable(
-    {
-        accept:     "[class~='card'][class~='ship']",
-        tolerance:  "pointer",
-        hoverClass: "hovering",
-        drop:       function (event, ui) {
-            alert("Gooooooooal!");
+
+    $( ".card" ).draggable();
+
+    $( ".space" ).filter(" .pony" ).droppable(
+        {
+            accept:     "[class~='card'][class~='pony']",
+            tolerance:  "pointer",
+            hoverClass: "hovering",
+            drop:       function(event, ui) {
+                var uri = ui.draggable.attr("src");
+
+                jQuery.post("/api/board", {card: uri},
+                    function(data, textStatus, jqXHR) {
+                        alert("Pony dropped:  " + data);
+                    }
+                );
+            }
         }
-    }
-);
+    );
+    $( ".space" ).filter(" .ship" ).droppable(
+        {
+            accept:     "[class~='card'][class~='ship']",
+            tolerance:  "pointer",
+            hoverClass: "hovering",
+            drop:       function (event, ui) {
+                alert("Gooooooooal!");
+            }
+        }
+    );
+});
+
 </script>
 
 @@ layouts/default.html.ep
