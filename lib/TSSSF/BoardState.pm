204200b19c73ca6aa9cc074099431e537652338d
darksuji <darksuji@gmail.com>
2014-05-27 12:36:04 -0700

    Remove DBIx::Class::Objects boilerplate

--- lib/TSSSF/BoardState.pm
+++ lib/TSSSF/BoardState.pm
@@ -2,9 +2,6 @@ package TSSSF::BoardState;
 use Moose;
 use namespace::autoclean;
 
-# this is optional. If you forget to include it, DBIx::Class::Objects will
-# inject this for you. However, it's good to have it here for
-# documentation purposes.
 extends 'DBIx::Class::Objects::Base';
 
 use JSON qw/to_json from_json/;
@@ -22,12 +19,6 @@ sub put_card {
     $self->update();
 
     return;
-}
-
-
-sub is_customer {
-    my $self = shift;
-    return defined $self->customer;
 }
 
 __PACKAGE__->meta->make_immutable;
